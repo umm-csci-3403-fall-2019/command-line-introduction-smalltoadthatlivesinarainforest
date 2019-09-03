60bc824377156dc36a94ebc30b0a4707c7431a62
robe1606 <robe1606@morris.umn.edu>
2019-09-03 18:22:24 -0500

    began work on a prototype of a bash file that will check all files and deletes them if the have the string "DELETE ME!"

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# need tar there is a flag for this that helps with cleaner extraction
+
+# should remove all files that contain DELETE ME!
+for f in $2
+       
+# should create a new compressed tar archive that contains all the files
+# not including those that were scratched
+
+#if there is a delete me, then we will not estract
+# else we will extract the file
